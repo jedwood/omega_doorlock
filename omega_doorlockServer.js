c72a84b76343e655964fcc824702ec5ce8778033
Jed Wood <jed@limechile.com>
2018-01-02 22:27:08 -0700

    parse that body for me

--- omega_doorlockServer.js
+++ omega_doorlockServer.js
@@ -3,6 +3,8 @@ var app = express();
 
 var omegaDoorlock = require('./omega_doorlock');
 
+app.use(express.bodyParser());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
